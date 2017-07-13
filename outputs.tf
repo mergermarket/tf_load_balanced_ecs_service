4f7691c4aaa4f5090ac620cabf033ecdf40a9276
grahamlyons <graham@grahamlyons.com>
2017-07-13 17:03:15 +0100

    Output the name of the service
    We'd like to be able to depend on this module from another module
    so expose the name of the service for us to use and acheive this
    goal.
    
    JIRA: PLAT-1045

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "target_group_arn" {
   value = "${aws_alb_target_group.target_group.arn}"
   description = "The ARN of the target group for use by ALB listener rules (e.g. as a parameter to the tf_alb_listener_rules module: https://github.com/mergermarket/tf_alb_listener_rules)."
 }
+
+output "name" {
+  value = "${aws_ecs_service.service.name}"
+}
